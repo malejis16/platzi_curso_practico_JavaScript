f7dc715611d4322c32339a212e1bb144259f29a0
Alejandra Nieto <malejis16@hotmail.com>
2022-01-03 16:12:55 -0500

    se agrega archivo de FigurasGeometricas.js

---
+++ FigurasGeometricas.js
@@ -0,0 +1,19 @@
+/*
+CUADRADO
+perimetro: borde del cuadrado, cúanto mide? se suman los lados 4xL. 
+area del cuadrado: L al cuadrado
+*/
+
+/*
+TRIANGULO
+perimetro: L1+L2+L3
+area: (B*A)/ 2
+*/
+
+/*
+CIRCULO
+radio: r
+diametro: 2*r
+perimetro: 2(pi)*r
+area: r*r*pi 
+*/
