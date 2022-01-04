8226d698856c79e8b94b74aab417ad27792fce52
Alejandra Nieto <malejis16@hotmail.com>
2022-01-04 17:18:29 -0500

    cambios en proyecto y html

--- FigurasGeometricas.js
+++ FigurasGeometricas.js
@@ -10,27 +10,6 @@ perimetro: L1+L2+L3
 area: (B*A)/ 2
 */
 
-var lado = 6;
-function perimetroCuadrado(lado) {
-  return lado * 4;
-}
-
-function perimetroTriangulo(lado1, lado2, lado3) {
-  return lado1 + lado2 + lado3;
-}
-
-function areaTriangulo(Base, Altura) {
-  return (Base * Altura) / 2;
-}
-
-function perimetroCirculo(Radio) {
-  return 2 * Math.PI * Radio;
-}
-
-function areaCirculo(Radio) {
-  return Radio * Radio * Math.PI;
-}
-
 /*
 CIRCULO
 radio: r
@@ -38,3 +17,66 @@ diametro: 2*r
 perimetro: 2(pi)*r
 area: r*r*pi 
 */
+
+console.group("Cuadrados");
+
+var lado = 6;
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+console.log(
+  "El perimetro del cuadrado es: " +
+    perimetroCuadrado(lado) +
+    " y su area es de: " +
+    areaCuadrado(lado)
+);
+
+console.groupEnd;
+
+console.group("Triangulos");
+
+var lado1 = 5;
+var lado2 = 8;
+var base = 10;
+var altura = 11;
+
+function perimetroTriangulo(lado1, lado2, lado3) {
+  return lado1 + lado2 + lado3;
+}
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+console.log(
+  "El perimetro del triángulo es: " +
+    perimetroTriangulo(lado1, lado2, base) +
+    " y su area es de: " +
+    areaTriangulo(base, altura)
+);
+
+console.groupEnd;
+
+console.group("Circulos");
+
+var radio = 5;
+
+function perimetroCirculo(radio) {
+  return 2 * Math.PI * radio;
+}
+
+function areaCirculo(radio) {
+  return radio * radio * Math.PI;
+}
+
+console.log(
+  "El perimetro del Círculo es: " +
+    perimetroCirculo(radio) +
+    " y su area es de: " +
+    areaCirculo(radio)
+);
+
+console.groupEnd;
