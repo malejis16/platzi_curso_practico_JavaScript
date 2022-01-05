3b4e01654bdb7cf724ce84c6c16d7c15ce886240
Alejandra Nieto <malejis16@hotmail.com>
2022-01-04 20:16:30 -0500

    finalización de codigo 040122

--- FigurasGeometricas.js
+++ FigurasGeometricas.js
@@ -1,48 +1,15 @@
-/*
-CUADRADO
-perimetro: borde del cuadrado, cúanto mide? se suman los lados 4xL. 
-area del cuadrado: L al cuadrado
-*/
-
-/*
-TRIANGULO
-perimetro: L1+L2+L3
-area: (B*A)/ 2
-*/
-
-/*
-CIRCULO
-radio: r
-diametro: 2*r
-perimetro: 2(pi)*r
-area: r*r*pi 
-*/
-
 console.group("Cuadrados");
-
-var lado = 7;
 
 function perimetroCuadrado(lado) {
   return lado * 4;
 }
+
 function areaCuadrado(lado) {
   return lado * lado;
 }
-console.log(
-  "El perimetro del cuadrado es: " +
-    perimetroCuadrado(lado) +
-    " y su area es de: " +
-    areaCuadrado(lado)
-);
 
 console.groupEnd;
-
 console.group("Triangulos");
-
-var lado1 = 5;
-var lado2 = 8;
-var base = 10;
-var altura = 11;
 
 function perimetroTriangulo(lado1, lado2, lado3) {
   return lado1 + lado2 + lado3;
@@ -52,18 +19,8 @@ function areaTriangulo(base, altura) {
   return (base * altura) / 2;
 }
 
-console.log(
-  "El perimetro del triángulo es: " +
-    perimetroTriangulo(lado1, lado2, base) +
-    " y su area es de: " +
-    areaTriangulo(base, altura)
-);
-
 console.groupEnd;
-
 console.group("Circulos");
-
-var radio = 5;
 
 function perimetroCirculo(radio) {
   return 2 * Math.PI * radio;
@@ -73,38 +30,47 @@ function areaCirculo(radio) {
   return radio * radio * Math.PI;
 }
 
-console.log(
-  "El perimetro del Círculo es: " +
-    perimetroCirculo(radio) +
-    " y su area es de: " +
-    areaCirculo(radio)
-);
-
 console.groupEnd;
 
+var inputLado = document.getElementById("inputLadoCuadrado");
+var inputBase = document.getElementById("inputBaseTriangulo");
+var inputAltura = document.getElementById("inputAlturaTriangulo");
+var inputLado1 = document.getElementById("inputLado1Triangulo");
+var inputLado2 = document.getElementById("inputLado2Triangulo");
+var inputBase = document.getElementById("inputBaseTriangulo");
+var inputRadio = document.getElementById("inputRadioCirculo");
+var inputRadio = document.getElementById("inputRadioCirculo");
+
 function calcularPerimetroCuadrado() {
-  var inputLado = document.getElementById("inputLadoCuadrado");
-  var lado = inputLado.value;
-  alert(perimetroCuadrado(lado));
+  alert(
+    "El perimetro del cuadrado es: " +
+      perimetroCuadrado(inputLado.value) +
+      " y su area es de: " +
+      areaCuadrado(inputLado.value)
+  );
 }
 
 function calcularAreaCuadrado() {
-  var inputLado = document.getElementById("inputLadoCuadrado");
-  var lado = inputLado.value;
-  alert(areaCuadrado(lado));
+  alert(areaCuadrado(inputLado.value));
 }
 
 function calcularAreaTriangulo() {
-  var inputLado1 = document.getElementById("inputLado1Triangulo");
-  var lado1 = inputLado1.value;
-  var inputLado2 = document.getElementById("inputLado2Triangulo");
-  var lado2 = inputLado2.value;
-  var inputBase = document.getElementById("inputBaseTriangulo");
-  var base = inputBase.value;
-  var inputAltura = document.getElementById("inputAlturaTriangulo");
-  var altura = inputAltura.value;
-  var inputRadio = document.getElementById("inputRadioCirculo");
-  var radio = inputRadio.value;
+  alert(areaTriangulo(inputBase.value, inputAltura.value));
+}
 
-  alert(areaTriangulo(base, altura));
+function calcularPerimetroTriangulo() {
+  alert(
+    perimetroTriangulo(
+      Number.parseInt(inputLado1.value),
+      Number.parseInt(inputLado2.value),
+      Number.parseInt(inputBase.value)
+    )
+  );
+}
+
+function calcularPerimetroCirculo() {
+  alert(perimetroCirculo(inputRadio.value));
+}
+function calcularAreaCirculo() {
+  alert(areaCirculo(inputRadio.value));
 }
--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 - Segundo paso: implementar las formulas en JavaScript
 - tercer paso: crear funciones
 - cuarto paso: integrar JavaScript con HTML
+
+Recordemos que:
+
+## CUADRADO
+
+perimetro: borde del cuadrado, cúanto mide? se suman los lados 4xL.
+area del cuadrado: L al cuadrado
+
+## TRIANGULO
+
+perimetro: L1+L2+L3
+area: (B\*A)/ 2
+
+## CIRCULO
+
+radio: r
+diametro: 2*r
+perimetro: 2(pi)*r
+area: r*r*pi
