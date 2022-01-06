389f803f98b0f7ad66023e3a2ab17d992658d0fe
Alejandra Nieto <malejis16@hotmail.com>
2022-01-05 19:29:57 -0500

    html descuentos en proceso

---
+++ descuentos.js
@@ -0,0 +1,8 @@
+var precio = getelementid;
+
+function buttonPriceDiscount() {
+  function calcularPrecioConDescuento(precio, descuento) {
+    const precioConDescuento = (precio * (100 - descuento)) / 100;
+    return precioConDescuento;
+  }
+}
